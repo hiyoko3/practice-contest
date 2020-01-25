1cb20a1648d818438e142d51798a73b80856f24d
hiyoko3 <sb.gm.0823@gmail.com>
2020-01-25 22:34:24 +0900

    abc-152 D.cpp

--- at-coder/abc-152/D.cpp
+++ at-coder/abc-152/D.cpp
@@ -1,14 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int main() {
-	string N;
-	cin >> N;
-	vector<int> digits(N.size());
+typedef long long ll;
+typedef pair<int, int> P;
 
-	for(int i = 0; i < N.size(); i++) {
-		digits[i] = (int)N[i];
+P getAB(int n) {
+	// n の先頭文字
+	int a = n % 10;
+	// n の末尾文字
+	int b = 0;
+	while (n) {
+		b = n;
+		n /= 10;
+	}
+	return P(a, b);
+}
+
+int main() {
+	int N;
+	ll result = 0;
+	map<P, int> pairs;
+	cin >> N;
+
+
+	for(int i = 1; i <= N; i++) {
+		P p = getAB(i);
+		// 出現pairをカウント
+		pairs[p] += 1;
 	}
 
-	
+	// 組み合わせ数の計算
+	for(int i = 1; i<= N; i++) {
+		P p = getAB(i);
+		P q(p.second, p.first);
+		result += pairs[q];
+	}
+
+	cout << result << endl;
+	return 0;
 }
