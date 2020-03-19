524a64f6d559e272190bb427dc0e8692dbea4137
hiyoko3 <sb.gm.0823@gmail.com>
2020-03-19 17:20:58 +0900

    Add abc-131 D.cpp

---
+++ at-coder/abc/abc-131/D.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+// increment
+#define rep_n(_1, _2, _3, NAME, ...) NAME
+#define rep_2(i, n) for (int i = 0; i < (int)(n); ++i)
+#define rep_3(i, initial, n) for (int i = (int)(initial); i < (int)(n); ++i)
+#define repeat(...) rep_n(__VA_ARGS__, rep_3, rep_2, rep_1)(__VA_ARGS__)
+// decrement
+#define rep_r_n(_1, _2, _3, NAME, ...) NAME
+#define rep_r_2(i, n) for (int i = (int)(n); i > 0; i--)
+#define rep_r_3(i, initial, n) for (int i = (int)(initial); i > (int)(n); i--)
+#define repeat_r(...) rep_r_n(__VA_ARGS__, rep_r_3, rep_r_2)(__VA_ARGS__)
+// define variable
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> P;
+const double EPS = numeric_limits<double>::epsilon();
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	map<int, int> m;
+
+	repeat(i, N) {
+		ll a, b;
+		cin >> a >> b;
+		m[b] += a;
+	}
+
+	string result = "Yes";
+	unsigned long long sum = 0;
+
+	for(auto i : m) {
+		sum += i.second;
+
+		if (sum > i.first) {
+			result = "No";
+			break;
+		}
+	}
+
+	cout << result << endl;
+	return 0;
+}
