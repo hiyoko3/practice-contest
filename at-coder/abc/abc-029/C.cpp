3fb6761e93d03743a4b13a4253d0bc741e7ce7ab
hiyoko3 <sb.gm.0823@gmail.com>
2020-06-25 22:00:11 +0900

    Added abc-029 C.cpp

--- at-coder/abc/abc-029/C.cpp
+++ at-coder/abc/abc-029/C.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
+
 #include <boost/multiprecision/cpp_int.hpp>
 
 // increment
 #define rep_n(_1, _2, _3, NAME, ...) NAME
 #define rep_2(i, n) for (int(i) = 0; (i) < (int)(n); ++(i))
-#define rep_3(i, initial, n) for (int(i) = (int)(initial); (i) < (int)(n); ++(i))
+#define rep_3(i, initial, n) \
+  for (int(i) = (int)(initial); (i) < (int)(n); ++(i))
 #define rep(...) rep_n(__VA_ARGS__, rep_3, rep_2, rep_1)(__VA_ARGS__)
 // decrement
 #define rep_r_n(_1, _2, _3, NAME, ...) NAME
 #define rep_r_2(i, n) for (int(i) = (int)(n); (i) > 0; (i)--)
-#define rep_r_3(i, initial, n) for (int(i) = (int)(initial); (i) > (int)(n); (i)--)
+#define rep_r_3(i, initial, n) \
+  for (int(i) = (int)(initial); (i) > (int)(n); (i)--)
 #define rep_r(...) rep_r_n(__VA_ARGS__, rep_r_3, rep_r_2)(__VA_ARGS__)
 // define type definition
 using namespace std;
 typedef long long ll;
-using mpint = boost::multiprecision::cpp_int; // 多倍長整数（任意精度）
+using mpint = boost::multiprecision::cpp_int;  // 多倍長整数（任意精度）
 typedef pair<int, int> P;
 
 // define construct variable
@@ -33,32 +36,25 @@ ll lcm(const ll a, const ll b) { return a / gcd(a, b) * b; }
 // char -> int
 int ctoi(char c) { return (int)(c - '0'); }
 // 素因数分解
-vector<pair<ll, ll>> prime_factorize(ll n)
-{
+vector<pair<ll, ll>> prime_factorize(ll n) {
   vector<pair<ll, ll>> p;
-  for (ll i = 2; i * i <= n; i++)
-  {
-    if ((n % i) != 0)
-      continue;
+  for (ll i = 2; i * i <= n; i++) {
+    if ((n % i) != 0) continue;
     pair<ll, ll> a = make_pair(i, 0);
-    while ((n % i) == 0)
-    {
+    while ((n % i) == 0) {
       n /= i;
       a.second += 1;
     }
     p.push_back(a);
   }
 
-  if (n != 1)
-    p.push_back(make_pair(n, 1));
+  if (n != 1) p.push_back(make_pair(n, 1));
   return p;
 }
 // 組み合わせの整合性を保つ（後ほど解析する）
-ll modinv(ll a)
-{
+ll modinv(ll a) {
   ll b = MOD, u = 1, v = 0;
-  while (b)
-  {
+  while (b) {
     ll t = a / b;
     a -= t * b;
     swap(a, b);
@@ -66,68 +62,63 @@ ll modinv(ll a)
     swap(u, v);
   }
   u %= MOD;
-  if (u < 0)
-  {
+  if (u < 0) {
     u += MOD;
   }
   return u;
 }
 // 順列
-ll permutation(const ll n, const ll m)
-{
+ll permutation(const ll n, const ll m) {
   ll result = 1;
-  for (ll i = n; i > n - m; i--)
-  {
+  for (ll i = n; i > n - m; i--) {
     result = (result * i) % MOD;
   }
   return result;
 }
 
 // 繰り返し n 乗法
-ll repeat_square(const ll n, const ll x)
-{
-  if (x == 0)
-    return 1;
+ll repeat_square(const ll n, const ll x) {
+  if (x == 0) return 1;
   ll res = repeat_square(n, x / 2);
   res = res * res % MOD;
-  if ((x % 2) == 1)
-    res = res * n % MOD;
+  if ((x % 2) == 1) res = res * n % MOD;
   return res;
 }
 // 組み合わせ nCm
-ll combination(ll n, ll m)
-{
+ll combination(ll n, ll m) {
   ll p = permutation(n, m);
   ll div = 1;
-  for (ll i = 2; i <= m; i++)
-  {
+  for (ll i = 2; i <= m; i++) {
     div = (div * i) % MOD;
   }
   return (p * modinv(div)) % MOD;
 }
 
 // アルファベットの取得
-string get_alphabet(ll n)
-{
+string get_alphabet(ll n) {
   n--;
   return string(1, 'a' + (n % 26));
 }
 
-int main()
-{
-  ll N;
-  cin >> N;
-
-  string ans = "";
-  vector<string> alpha;
-
-  while (N > 0)
-  {
-    N--;
-    ans = get_alphabet(N) + ans;
-    N /= 26;
+map<string, int> ans;
+void dfs(string s, int N) {
+  if (s.length() == N) {
+    ans[s]++;
+    return;
   }
+  dfs(s+"a", N);
+  dfs(s+"b", N);
+  dfs(s+"c", N);
+  return;
+}
 
-  cout << ans << endl;
+int main() {
+  int N;
+  cin >> N;
+  dfs("", N);
+
+  for(auto itr: ans) {
+    cout << itr.first << endl;
+  }
   return 0;
 }
